da465f41e25a1e3311b8934689cdd6aa4f071c4d
ridzuan5757 <ridzuan5757@protonmail.com>
2023-11-17 23:34:40 +0800

    unit test for fobj

--- lib/fobj.ts
+++ lib/fobj.ts
@@ -1,6 +1,6 @@
 import { IXmlNode } from "./interface";
 
-export function xmlToObj(node: IXmlNode): string {
+export function objToXml(node: IXmlNode): string {
   const attributes = Object.keys(node.attributes)
     .map((key) => `${key}="${node.attributes[key]}"`)
     .join(" ");
@@ -9,7 +9,8 @@ export function xmlToObj(node: IXmlNode): string {
     return `<${node.startTag}${attributes ? " " + attributes : ""}/>`;
   }
 
-  const childrenXml = node.children.map((child) => xmlToObj(child)).join("");
-  return `<${node.startTag}${attributes ? " " + attributes : ""
-    }>${childrenXml}</${node.nodeName}>`;
+  const childrenXml = node.children.map((child) => objToXml(child)).join("");
+  return `<${node.startTag}${
+    attributes ? " " + attributes : ""
+  }>${childrenXml}</${node.nodeName}>`;
 }
--- lib/index.ts
+++ lib/index.ts
@@ -1,2 +1,3 @@
 export * from "./fxml";
+export * from "./fobj";
 export * from "./interface";
